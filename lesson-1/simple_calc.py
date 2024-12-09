48ddbae7fe26362869cc7ac60d46ed19f0ff9d98
Hikmatyor Azatov <azatovhikmatyor@gmail.com>
2024-12-09 21:47:54 +0500

    some changes

---
+++ lesson-1/simple_calc.py
@@ -0,0 +1,5 @@
+a = float(input("a = "))
+b = float(input("b = "))
+
+c = a + b
+print('a + b =', c)
---
+++ lesson-1/third.py
@@ -0,0 +1,8 @@
+
+len = 5
+my_second_number = 10
+
+print(len + my_second_number)
+
+print(len('hello'))
+
